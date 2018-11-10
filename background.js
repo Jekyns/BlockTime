50facb997146a79b29d5b8f31b3d22815023e0aa
Jekyns <Jekyns@yandex.ru>
2018-11-10 14:02:01 +0300

    stable v1.0

---
+++ background.js
@@ -0,0 +1,81 @@
+﻿
+chrome.browserAction.onClicked.addListener(function(tab) {//подключаю jquery к контенту
+    chrome.tabs.executeScript(null, {file:"jquery.min.js"}, function() {
+        chrome.tabs.executeScript(null, {file:"content.js"});
+    });
+});
+function getCookies(domain, name, callback) {
+				chrome.cookies.get({"url": domain, "name": name}, function(cookie) {
+				if(callback) {
+					callback(cookie.value);
+				}
+				});
+			}
+var a = 30;
+var url = "";
+var timecodes=7;
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.method == "x2") {
+        a = 10;
+		chrome.cookies.set({
+			"name": "AdSpeed",
+			"url": "https://www.youtube.com/",
+			"expirationDate":new Date().getTime()/1000 + 3.154e+7,//устанавливаем истечение куки через год
+			"value": "x2",
+			});
+    }
+    if (message.method == "x4") {
+      chrome.cookies.set({
+			"name": "AdSpeed",
+			"url": "https://www.youtube.com/",	//http://developer.chrome.com/extensions/cookies.html
+			"expirationDate":new Date().getTime()/1000 + 3.154e+7,//устанавливаем истечение куки через год
+			"value": "x4",
+			});
+    }
+    if (message.method == "moment") {
+       chrome.cookies.set({
+			"name": "AdSpeed",
+			"url": "https://www.youtube.com/",
+			"expirationDate":new Date().getTime()/1000 + 3.154e+7,//устанавливаем истечение куки через год
+			"value": "moment",
+			});
+        
+    }
+    if (message.method == "ret") {
+		var expper;
+		
+			getCookies("https://www.youtube.com/", "AdSpeed", function(value) {
+				//alert(value);
+				 window.expper=value;
+				 
+			});
+			//alert(window.expper);
+			//sendResponse(expper);
+    }
+	if (message.method == "geturl") {
+		sendResponse(url);
+    }
+	if (message.method == "serverTime") {
+		//alert(url);
+		var Site={serverName:"port.16mb.com"}; /* адрес сайта */
+
+			//alert(2);
+			$.ajax({
+				type:'GET',
+				  async: false,
+				url:"http://"+Site.serverName+"/primer/ajaxform/ajax.php",
+				data:{url:message.url},//передаем на сервер текущую юрл
+				response:'text',//тип возвращаемого ответа text либо xml
+				cache:false,
+				success:function (data) {//возвращаемый результат от сервера(тайм коды в джсон строке)
+				//alert(data);
+				timecodes=data;// переобразовываем джсон строку
+				
+				}
+
+			});
+			sendResponse(timecodes);//передаем полученный массив в content
+			
+		
+    }
+});
---
+++ content.js
@@ -0,0 +1,112 @@
+﻿/**
+*Функция берет куки указанной строки
+*@(name) {string} Имя кука
+*@(matches) {string} возвращает значение кука
+*/
+function getCookie(name) {
+	var matches = document.cookie.match(new RegExp(
+		"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+	));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+/**
+*Функция считает дробную часть из числа
+*@(num) {float} число для получения дробной части
+*@(result) {float} возвращает дробную часть 
+*/
+function getDecimal(num) {//получение дробной части
+	var result;
+	var initialNum = Math.abs(num);
+	var roundedNum = Math.round(initialNum);
+
+	if (roundedNum > initialNum) {
+		result = roundedNum - initialNum - 1;
+		result = Math.abs(result);
+		result = +result.toFixed(10);
+	}
+	else {
+		result = initialNum - roundedNum;
+		result = +result.toFixed(10);
+	}
+	return result;
+}
+var remUrl = document.location.href;
+var curUrl = document.location.href.split('&');//бепем текущую ссылку
+var youtube = "https://www.youtube.com/watch"
+var i = 0;//счетчик таймкодов
+var codetimes = 0;
+var codetimeslength = 0;
+var AdSpeedCook = getCookie("AdSpeed");
+chrome.runtime.sendMessage({ method: "serverTime", url: curUrl[0] }, function (responce) {//пытаемся взять тайм коды по текущей ссылке
+	codetimes = JSON.parse(responce);
+	codetimeslength = codetimes.length;
+});
+function lul() {
+	if (remUrl != document.location.href) {//если ссылка запустилась а content js нет
+		remUrl = l = document.location.href;
+		curUrl = document.location.href.split('&');//меняем значение текущей ссылке на действительно текущую 
+		chrome.runtime.sendMessage({ method: "serverTime", url: curUrl[0] }, function (responce) {//пытаемся взять тайм коды по текущей ссылке
+			codetimes = JSON.parse(responce);
+			codetimeslength = codetimes.length;
+			i = 0;
+
+		});
+
+	}
+}
+setInterval(lul, 4);
+
+function time() {
+	AdSpeedCook = getCookie("AdSpeed");
+
+	javascript: document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = 1.0;
+	while (codetimes[i]) {
+		if ((document.getElementsByClassName("video-stream html5-main-video")[0].currentTime < Math.trunc(codetimes[i]['stop']) * 60 + getDecimal(codetimes[i]['stop']) * 100)//если меньше конца таймкода
+			&& (document.getElementsByClassName("video-stream html5-main-video")[0].currentTime > Math.trunc(codetimes[i]['start']) * 60 + getDecimal(codetimes[i]['start']) * 100)) {//если больше начала таймкода
+			switch (AdSpeedCook) {
+				case 'x2':
+
+					javascript: document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = 2.0;
+
+					break;
+				case 'x4':
+
+					javascript: document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = 4.0;
+
+					break;
+				case 'moment':
+					javascript: document.getElementsByClassName("video-stream html5-main-video")[0].currentTime = Math.trunc(codetimes[i]['stop']) * 60 + getDecimal(codetimes[i]['stop']) * 100;
+
+					break;
+				default:
+					alert('Я таких значений не знаю');
+			}
+
+
+
+
+		}
+		i++;
+	}
+	i = 0;
+}
+
+
+
+setInterval(time, 4);
+
+function eeep() {
+	let sec = Math.trunc(document.getElementsByClassName("video-stream html5-main-video")[0].currentTime);
+	let min = Math.trunc(document.getElementsByClassName("video-stream html5-main-video")[0].currentTime);
+	if (sec >= 60) {
+		sec = sec % 60;
+	}
+	time = Math.floor(min / 60) + (sec / 100);
+
+	chrome.runtime.sendMessage({ method: "newlist", message: time, url: document.location.href })
+	chrome.runtime.sendMessage({ method: "checklink", message: document.getElementsByClassName("video-stream html5-main-video")[0].currentTime }, function (responce) {
+	});
+
+
+}
+setInterval(eeep, 1000);
